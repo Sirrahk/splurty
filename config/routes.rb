fd932bcd90c512db73a20b209596a20fabcf9610
Keara Harris <keara.s.harris@gmail.com>
2019-08-23 21:26:58 +0000

    About Page with bio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
     root 'quotes#index'
     resource :quotes
-    
- # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'about', to: 'quotes#about'
+
 end
 
